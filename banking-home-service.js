fae23f5d0df7802829735d71ae10f4fcbb1c8888
jim@jimdonaghy.com <jim@jimdonaghy.com>
2023-05-20 22:19:44 -0700

    first commit

---
+++ banking-home-service.js
@@ -0,0 +1,52 @@
+
+let express = require("express");
+let app = express();app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
+let RESTClient = require('node-rest-client').Client;
+let restClient = new RESTClient();
+
+let response = "{}";
+
+
+function asyncFetchAccounts() {
+    return new Promise(resolve => {
+        restClient.get("http://localhost:8082/accounts", (data,response)=> {
+         resolve(data)
+       })
+    })
+ }
+
+ function asyncFetchCustomers() {
+    return new Promise(resolve => {
+        restClient.get("http://localhost:8080/customers", (data,response)=> {
+         resolve(data)
+       })
+    })
+ }
+
+ async function getData(){
+
+    let accounts = await asyncFetchAccounts();
+    let customers =  await asyncFetchCustomers();
+    
+    response = accounts.concat(customers);
+
+    return response;
+ }
+
+
+app.get("/url", (req, res, next) => {
+    
+    getData().then(
+        (data) => { res.send(data);
+    });
+    
+    
+});
+
+
+
+
+
